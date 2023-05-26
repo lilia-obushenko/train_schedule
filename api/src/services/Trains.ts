df90de57cc50bd4141c34cfa8ffdf4d9879a23f5
Lilia Obushenko <liliaobushenko@gmail.com>
2023-05-26 09:54:16 +0300

    little fix with case

--- api/src/services/Trains.ts
+++ api/src/services/Trains.ts
@@ -3,12 +3,20 @@
 import { Train } from '../models/Train';
 import { TrainType } from '../typedefs';
 
+const prepareSearch = (value: string) => {
+  const preparedFrom = value.toLowerCase();
+  return `${preparedFrom.slice(0, 1).toUpperCase()}${preparedFrom.slice(1)}`
+};
+
 const getAll = async(from?: string, to?:string) => {
   let products;
 
   if (from && to) {
     products = await Train.findAll({
-      where: { from, to },
+      where: { 
+        from: prepareSearch(from), 
+        to: prepareSearch(to),
+      },
       order: ['dispatch'],
     });
   } else {
